3afb63a2576acd3bc137f157ce7c6106609cc536
Alexdelia <alexandre.delille.57@gmail.com>
2024-06-09 23:36:19 +0200

    learning `Polars`

--- src/describe/src/describe/mod.rs
+++ src/describe/src/describe/mod.rs
@@ -6,6 +6,14 @@ use polars::prelude::{DataFrame, PolarsResult};
 use crate::Args;
 
 pub fn describe(df: DataFrame, args: &Args) -> PolarsResult<()> {
+	lazy_frame(df.clone());
+
 	let table = compute::compute(df, args)?;
 	present::present(table, args)
+}
+
+use polars::prelude::*;
+
+fn lazy_frame(df: DataFrame) {
+	dbg!(df.clone().lazy().select(&[sum("*")]).collect().unwrap());
 }
