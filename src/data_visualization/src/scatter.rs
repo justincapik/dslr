7e30f881af8b33c3f0b0e095fd50433c477c6542
adelille <adelille@student.42.fr>
2024-10-18 20:36:50 +0200

    scatter

---
+++ src/data_visualization/src/scatter.rs
@@ -0,0 +1,54 @@
+use std::{error::Error, path::Path};
+
+use plotly::Plot;
+use polars::prelude::*;
+
+use visualize::{
+	annotation, args, feature, image, layout, populate, trace, Label, PlotType, LABEL_NAME,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let args = args::parse("scatter_plot.png");
+
+	let mut dataset = load::load(args.csv)?;
+
+	populate::date(&mut dataset)?;
+
+	plot(dataset, args.output)
+}
+
+fn plot<P: AsRef<Path>>(dataset: DataFrame, output: P) -> Result<(), Box<dyn Error>> {
+	let mut plot = Plot::new();
+
+	let mut layout = layout::build(PlotType::Scatter);
+
+	for df_label in dataset.partition_by([LABEL_NAME], true)? {
+		let mut plot_index = 1;
+
+		let (label, color) = Label::extract(&df_label)?;
+
+		for series in df_label.get_columns() {
+			let Ok(col) = feature::parse(series) else {
+				continue;
+			};
+
+			plot.add_trace(trace::scatter(col, &label, color, plot_index));
+
+			layout.add_annotation(annotation(plot_index, &label));
+
+			plot_index += 1;
+		}
+	}
+
+	plot.set_layout(layout);
+
+	plot.write_image(
+		output,
+		image::FORMAT,
+		image::WIDTH,
+		image::HEIGHT,
+		image::SCALE,
+	);
+
+	Ok(())
+}
--- src/data_visualization/src/trace.rs
+++ src/data_visualization/src/trace.rs
@@ -1,4 +1,8 @@
-use plotly::{color::NamedColor, common::Marker, Histogram};
+use plotly::{
+	color::NamedColor,
+	common::{Marker, Mode},
+	Histogram, Scatter,
+};
 
 pub fn histogram(
 	col: Vec<f64>,
@@ -8,6 +12,23 @@ pub fn histogram(
 ) -> Box<Histogram<f64>> {
 	Histogram::new(col)
 		.marker(Marker::new().color(color))
+		.x_axis(format!("x{plot_index}"))
+		.y_axis(format!("y{plot_index}"))
+		.name(label)
+}
+
+pub fn scatter(
+	col: Vec<f64>,
+	label: &str,
+	color: NamedColor,
+	plot_index: usize,
+) -> Box<Scatter<f64, f64>> {
+	let t: Vec<f64> = col.iter().copied().rev().collect();
+	let y = col;
+
+	Scatter::new(t, y)
+		.mode(Mode::Markers)
+		.marker(Marker::new().color(color).size(3))
 		.x_axis(format!("x{plot_index}"))
 		.y_axis(format!("y{plot_index}"))
 		.name(label)
